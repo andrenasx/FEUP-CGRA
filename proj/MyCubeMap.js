02d560d6f7813dadfe07a9db273e66e09ab3827d
andrenasx <andre2000.an@gmail.com>
2020-04-10 21:59:25 +0100

    Inital background added

---
+++ proj/MyCubeMap.js
@@ -0,0 +1,112 @@
+/**
+ * MyUnitCubeQuad
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyCubeMap extends CGFobject {
+	constructor(scene) {
+        super(scene);
+
+        this.quad = new MyQuad(this.scene);
+        this.initTexture(scene);
+    }
+
+    initTexture(scene){
+        //From Z=inf
+        this.front = new CGFappearance(this.scene);
+        this.front.setAmbient(0.9, 0.9, 0.9, 1);
+        this.front.setDiffuse(0.0, 0.0, 0.0, 1);
+        this.front.setSpecular(0.0, 0.0, 0.0, 1);
+        this.front.setShininess(10.0);
+        this.front.loadTexture('images/split_cubemap/front.png');
+        this.front.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.left = new CGFappearance(this.scene);
+        this.left.setAmbient(0.9, 0.9, 0.9, 1);
+        this.left.setDiffuse(0.0, 0.0, 0.0, 1);
+        this.left.setSpecular(0.0, 0.0, 0.0, 1);
+        this.left.setShininess(10.0);
+        this.left.loadTexture('images/split_cubemap/left.png');
+        this.left.setTextureWrap('REPEAT', 'REPEAT');
+        
+        this.right = new CGFappearance(this.scene);
+        this.right.setAmbient(0.9, 0.9, 0.9, 1);
+        this.right.setDiffuse(0.0, 0.0, 0.0, 1);
+        this.right.setSpecular(0.0, 0.0, 0.0, 1);
+        this.right.setShininess(10.0);
+        this.right.loadTexture('images/split_cubemap/right.png');
+        this.right.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.back = new CGFappearance(this.scene);
+        this.back.setAmbient(0.9, 0.9, 0.9, 1);
+        this.back.setDiffuse(0.0, 0.0, 0.0, 1);
+        this.back.setSpecular(0.0, 0.0, 0.0, 1);
+        this.back.setShininess(10.0);
+        this.back.loadTexture('images/split_cubemap/back.png');
+        this.back.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.top = new CGFappearance(this.scene);
+        this.top.setAmbient(0.9, 0.9, 0.9, 1);
+        this.top.setDiffuse(0.0, 0.0, 0.0, 1);
+        this.top.setSpecular(0.0, 0.0, 0.0, 1);
+        this.top.setShininess(10.0);
+        this.top.loadTexture('images/split_cubemap/top.png');
+        this.top.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.bot = new CGFappearance(this.scene);
+        this.bot.setAmbient(0.9, 0.9, 0.9, 1);
+        this.bot.setDiffuse(0.0, 0.0, 0.0, 1);
+        this.bot.setSpecular(0.0, 0.0, 0.0, 1);
+        this.bot.setShininess(10.0);
+        this.bot.loadTexture('images/split_cubemap/bottom.png');
+        this.bot.setTextureWrap('REPEAT', 'REPEAT');
+    }
+
+    display(){
+        this.scene.pushMatrix();
+        this.scene.scale(50,50,50);
+
+        this.front.apply();
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, -0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        this.back.apply();        
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, 0.5);
+        this.scene.rotate(Math.PI, 0,1,0);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        this.left.apply();
+        this.scene.pushMatrix();
+        this.scene.translate(-0.5, 0, 0);
+        this.scene.rotate(Math.PI/2, 0,1,0);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        this.right.apply();
+        this.scene.pushMatrix();
+        this.scene.translate(0.5, 0, 0);
+        this.scene.rotate(-Math.PI/2, 0,1,0);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        this.top.apply();
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0.5, 0);
+        this.scene.rotate(Math.PI/2, 1,0,0);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        this.bot.apply();
+        this.scene.pushMatrix();
+        this.scene.translate(0, -0.5, 0);
+        this.scene.rotate(-Math.PI/2, 1,0,0);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        this.scene.popMatrix();
+    }
+}
