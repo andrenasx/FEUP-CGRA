ccd37f9eff0e75267d55ff747c2594b9d3f6f21e
anatcruz <anateresacruz2000@gmail.com>
2020-04-13 15:47:42 +0100

    Really fixed!

--- proj/MyVehicle.js
+++ proj/MyVehicle.js
@@ -107,7 +107,7 @@ class MyVehicle extends CGFobject {
         this.scene.rotate(this.angle*Math.PI/180, 0, 1, 0);
 
         this.scene.translate(0, 0, -1);
-        this.scene.rotate(90**Math.PI/180, 1, 0, 0);
+        this.scene.rotate(90*Math.PI/180, 1, 0, 0);
         super.display();
         this.scene.popMatrix();
     }
