32d92b81818674dc39f721ef532f10a9a466a3ad
andrenasx <andre2000.an@gmail.com>
2020-05-18 11:00:32 +0100

    removed initTexture

--- proj/MyCubeMapOut.js
+++ proj/MyCubeMapOut.js
@@ -8,17 +8,6 @@ class MyCubeMapOut extends CGFobject {
         super(scene);
 
         this.quad = new MyQuad(this.scene);
-        //this.initTexture(scene);
-    }
-
-    initTexture(scene){
-        this.tex = new CGFappearance(scene);
-        this.tex.setAmbient(0.9, 0.9, 0.9, 1);
-        this.tex.setDiffuse(0.0, 0.0, 0.0, 1);
-        this.tex.setSpecular(0.0, 0.0, 0.0, 1);
-        this.tex.setShininess(10.0);
-        this.tex.loadTexture('textures/crate0.png');
-        this.tex.setTextureWrap('CLAMP_TO_EDGE', 'CLAMP_TO_EDGE');
     }
 
     display(){
